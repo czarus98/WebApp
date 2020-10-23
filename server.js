b33082ea2cafd42455b978728cffdd400aa53bb9
czarus98 <czarek.wojcik98@gmail.com>
2020-10-23 17:38:11 +0200

    Created server origin and gitignore file

---
+++ server.js
@@ -0,0 +1,24 @@
+
+let http = require('http');
+let node_static = require('node-static')
+
+let httpServer = http.createServer()
+let fileServer = new node_static.Server('./public')
+
+let serveError = (res, code) => {
+    res.writeHead(code, { "Content-Type": "text/plain; charset=utf-8" })
+    res.write('Error')
+    res.end()
+
+}
+
+httpServer.on('request', function (req, res) {
+        console.log(req.method, req.url)
+        if (/^\/rest/.test(req.url)) {
+            serveError(res, 403)
+        }
+        fileServer.serve(req, res)
+    }
+)
+
+httpServer.listen(8080)
