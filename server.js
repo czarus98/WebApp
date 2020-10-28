54d2ba03bb1df9b5cdbfb8368edd74ecffc28534
czarus98 <czarek.wojcik98@gmail.com>
2020-10-28 16:23:20 +0100

    init commit on additional

--- server.js
+++ server.js
@@ -6,18 +6,18 @@ let httpServer = http.createServer()
 let fileServer = new node_static.Server('./public')
 
 let serveError = (res, code) => {
-    res.writeHead(code, { "Content-Type": "text/plain; charset=utf-8" })
+    res.writeHead(code, { "Content-Type": 'text/plain; charset=utf-8' })
     res.write('Error')
     res.end()
-
 }
 
 httpServer.on('request', function (req, res) {
         console.log(req.method, req.url)
         if (/^\/rest/.test(req.url)) {
             serveError(res, 403)
+        } else {
+            fileServer.serve(req, res)
         }
-        fileServer.serve(req, res)
     }
 )
 
