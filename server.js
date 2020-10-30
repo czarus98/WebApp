67d85293af63522a98ff35ba3f051a0b8393b62b
czarus98 <czarek.wojcik98@gmail.com>
2020-10-30 16:49:26 +0100

    Deleted /zero endpoint

--- server.js
+++ server.js
@@ -1,23 +1,47 @@
 
 let http = require('http');
 let node_static = require('node-static')
+let url = require('url')
 
 let httpServer = http.createServer()
 let fileServer = new node_static.Server('./public')
 
 let serveError = (res, code) => {
-    res.writeHead(code, { "Content-Type": "text/plain; charset=utf-8" })
+    res.writeHead(code, { "Content-Type": 'text/plain; charset=utf-8' })
     res.write('Error')
     res.end()
+}
 
+let person={
+    firstName: 'Czarek',
+    lastName: 'Wojcik',
+    description: 'tralala',
+    year: 1998
 }
 
 httpServer.on('request', function (req, res) {
         console.log(req.method, req.url)
-        if (/^\/rest/.test(req.url)) {
-            serveError(res, 403)
+        var parsed = url.parse(req.url, true)
+        switch(parsed.pathname) {
+            case '/get':
+                res.writeHead(200, { "Content-Type": 'application/json; charset=utf-8' })
+                res.write(JSON.stringify(person))
+                res.end()
+                break
+            case '/set':
+                person.firstName=parsed.query.firstName
+                person.lastName=parsed.query.lastName
+                person.year=parseInt(parsed.query.year)
+                if(isNaN(person.year)) {
+                    person.year=2000
+                }
+                res.writeHead(200, { "Content-Type": 'application/json; charset=utf-8' })
+                res.write(JSON.stringify(person))
+                res.end()
+                break
+            default:
+                fileServer.serve(req, res)
         }
-        fileServer.serve(req, res)
     }
 )
 
