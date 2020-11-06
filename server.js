4b3659ca582dcede56d15be8975a13ac4ee6feb0
czarus98 <czarek.wojcik98@gmail.com>
2020-11-06 16:22:53 +0100

    Added helper methods

--- server.js
+++ server.js
@@ -7,58 +7,56 @@ let fileServer = new node_static.Server('./public')
 let person = {
     firstName: 'Czarek',
     lastName: 'Wojcik',
-    amount: 0,
+    amount: 0.0,
     year: 1998
 }
 
+let serveJson = function (res, obj, code = 200) {
+    res.writeHead(200, {"Content-Type": 'application/json; charset=utf-8'})
+    res.write(JSON.stringify(obj))
+    res.end()
+}
+
+let serveError = function (res, code) {
+    serveJson(res, {error: 'Error occured'}, code)
+}
+
 httpServer.on('request', function (req, res) {
-    let payload = '';
-    req.on('data', function (data) {
+        let payload = '';
+        req.on('data', function (data) {
             payload += data
         }).on('end', function () {
-        let data = {};
-        try {
-            data = JSON.parse(payload);
-        } catch (ex) {
-        }
-        let parsed = url.parse(req.url, true);
-        switch (parsed.pathname) {
+            let data = {};
+            try {
+                data = JSON.parse(payload);
+            } catch (ex) {
+            }
+            let parsed = url.parse(req.url, true);
+            switch (parsed.pathname) {
                 case '/person':
                     switch (req.method) {
                         case 'GET':
-                            res.writeHead(200, {"Content-Type": 'application/json; charset=utf-8'})
-                            res.write(JSON.stringify(person))
-                            res.end()
+                            serveJson(res, person)
                             break
                         case 'PUT':
-                            person.firstName = data.firstName
-                            person.lastName = data.lastName
-                            person.year = parseInt(data.year)
-                            if (isNaN(person.year)) {
-                                person.year = 2000
-                            }
-                            res.writeHead(200, {"Content-Type": 'application/json; charset=utf-8'})
-                            res.write(JSON.stringify(person))
-                            res.end()
+                            Object.assign(person, data)
+                            serveJson(res, person)
                             break
                         case 'POST':
                             let delta = parseFloat(data.delta)
-                            if(isNaN(delta))
-                            {
-
+                            if (isNaN(delta)) {
+                                serveError(res, 400);
                             } else {
                                 person.amount += delta
-                                res.writeHead(200, {"Content-Type": 'application/json; charset=utf-8'})
-                                res.write(JSON.stringify(person))
-                                res.end()
+                                serveJson(res, person)
                             }
                             break
                         case 'DELETE':
-                            person.amount=0
-                            res.writeHead(200, {"Content-Type": 'application/json; charset=utf-8'})
-                            res.write(JSON.stringify(person))
-                            res.end()
+                            person.amount = 0
+                            serveJson(res, person)
                             break
+                        default:
+                            serveError(res, 405);
                     }
                     break
                 default:
